91a9b7b60d7a757bf904b260f09a4ffc99f28dfa
apelbeku <syauqyalhaq5@gmail.com>
2019-12-09 09:55:25 +0700

    pembersihan template

--- template/navbar.php
+++ template/navbar.php
@@ -135,9 +135,8 @@
             </ul>
           </li>
           <!-- Control Sidebar Toggle Button -->
-          <li>
-            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-          </li>
+
+          
         </ul>
       </div>
     </nav>
